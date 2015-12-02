8c86799e08f436f88bc19f32da132fc219b091e6
rembo10 <rembo10@headphones>
2015-12-01 22:15:21 -0800

    Basic command line parsing

--- headphones.js
+++ headphones.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
+var commander = require('commander');
+var env = require('./lib/env');
 var loader = require('./lib/loader');
 var logger = require('./lib/logger');
 
 function main() {
+
+  commander
+    .usage('[options]')
+    .option('-q, --quiet', 'Turn off console logging')
+    .option('-v, --verbose', 'Increase console logging')
+    .option('-p, --port <portNumber>', 'Manually specify port (default: 8181)', parseInt)
+    .parse(process.argv);
+
+  env.argv = commander;
+
   logger.initialize();
   loader.load();
 }
