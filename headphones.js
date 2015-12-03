351f969978b90c362f47619e028944ad23cd40cc
rembo10 <rembo10@headphones>
2015-12-02 18:46:35 -0800

    Initial argument parsing

--- headphones.js
+++ headphones.js
@@ -1,24 +1,80 @@
 #!/usr/bin/env node
 var commander = require('commander');
 var env = require('./lib/env');
+var events = require('./lib/events');
+var fs = require('fs');
 var loader = require('./lib/loader');
 var logger = require('./lib/logger');
+var mkdirp = require('mkdirp');
+var path = require('path');
 
-function main() {
-
+function parseArgs(args) {
   commander
     .usage('[options]')
     .option('-q, --quiet', 'Turn off console logging')
-    .option('-v, --verbose', 'Increase console logging')
-    .option('-p, --port <portNumber>', 'Manually specify port (default: 8181)', parseInt)
-    .parse(process.argv);
+    .option('-v, --verbose', 'Include debug level logging in console')
+    .option('-p, --port <portNumber>', 'Manually specify port (default: 8181)',
+            parseInt)
+    .option('--dataDir <path>', 'Specify path to program data directory')
+    .option('-c, --config <path>', 'Specify path to configuration file')
+    .parse(args);
+  return commander;
+}
 
-  env.argv = commander;
+function getDataDir(platform) {
+  switch(platform) {
+    case 'darwin':
+      return path.join(process.env.HOME, 'Library', 'Application Support',
+                        'Headphones');
+    case 'nt':
+      return path.join(process.env.APPDATA, 'Headphones');
+    case 'linux':
+      return path.join(process.env.HOME, '.headphones');
+    case 'freebsd':
+      return path.join('usr', 'local', 'headphones', 'data');
+  }
+}
+
+function readConfig(path) {
+  try {
+    return JSON.parse(fs.readFileSync(path, 'utf8'));
+  }
+  catch (err){
+    return null;
+  }
+}
+
+function writeConfig() {
+  fs.writeFileSync(path.join(env.dataDir, 'settings.json'),
+                  JSON.stringify(env.config, null, 2));
+}
+
+function main(argv) {
+  env.argv = argv;
+  env.dirName = __dirname;
+
+  env.dataDir = env.argv.datadir || getDataDir(process.platform);
+
+  try {
+    mkdirp.sync(env.dataDir);
+  } catch (err) {
+    console.error('Error creating data directory: ' + err);
+    process.exit(0);
+  }
+
+  var configPath = env.argv.config || path.join(env.dataDir, 'settings.json');
+  env.config = readConfig(configPath);
 
   logger.initialize();
   loader.load();
 }
 
+process.on('SIGINT', function() {
+  writeConfig();
+  process.exit(0);
+});
+
 if (require.main === module) {
-  main();
+  var argv = parseArgs(process.argv);
+  main(argv);
 }
